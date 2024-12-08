908bc19fdd42fefffc360766ff80863342ea8ece
SobakaBekka <151512975+SobakaBekka@users.noreply.github.com>
2024-12-08 23:08:58 +0100

    end

---
+++ OnlineSupermarket/Views/Home/ProductsByCategory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineSupermarket.Views.Home
+{
+    public class ProductsByCategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
